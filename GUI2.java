3a379d83ec54503631f9406ea56e22a738d6969a
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-28 08:32:32 -0300

    Update GUI2.java

--- GUI2.java
+++ GUI2.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 class GUI2{
       public static void main(String args[]){
-           JFrame frame = new JFrame("My First GUI");
+           JFrame frame = new JFrame("Election");
            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
            frame.setSize(300,300);
-          JButton button1 = new JButton("Button 1");
-          JButton button2 = new JButton("Button 2");
+          JButton button1 = new JButton("Vote yes");
+          JButton button2 = new JButton("Vote no");
           frame.getContentPane().add(button1);
           frame.getContentPane().add(button2);
           frame.setVisible(true);
