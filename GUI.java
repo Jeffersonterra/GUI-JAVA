4a9eb3712cb91a8acc1c1b3a6fe58fb4c46d40c6
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-28 08:33:01 -0300

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -2,10 +2,10 @@ import javax.swing.*;
 
 class GUI {
     public static void main(String args[]) {
-        JFrame frame = new JFrame("My First GUI");
+        JFrame frame = new JFrame("Election");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
-        JButton button = new JButton("Press");
+        JButton button = new JButton("Press here to vote");
         frame.getContentPane().add(button); // Adds Button to content pane of frame
         frame.setVisible(true);
     }
