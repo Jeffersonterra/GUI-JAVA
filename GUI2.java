5442360ed657f75ceb9188de5e95cbbda9502320
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-29 08:57:18 -0300

    Update GUI2.java

--- GUI2.java
+++ GUI2.java
@@ -7,7 +7,7 @@ class GUI2{
           JButton button1 = new JButton("Vote yes");
           JButton button2 = new JButton("Vote no");
             button1.setBounds(10, 20 , 80, 25);
-            button2..setBounds(100, 20 , 80, 25);
+            button2.setBounds(100, 20 , 80, 25);
           frame.getContentPane().add(button1);
           frame.getContentPane().add(button2);
           frame.setVisible(true);
