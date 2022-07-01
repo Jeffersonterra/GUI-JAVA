a93e64b2b50eb4887ce2f31d50e3beea23b4e3a4
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-01 12:40:40 -0300

    Update GUIRecatanglecalc.java

--- GUIRecatanglecalc.java
+++ GUIRecatanglecalc.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,7 +7,7 @@ import javax.swing.*;
 public class GUIRecatanglecalc implements ActionListener {
     private static JTextField mathText;
     private static JTextField mathtwoText;
-
+    private static JLabel result;
     private static JLabel userlabel;
     private static JButton button;
     int countyes = 0;
@@ -16,7 +15,7 @@ public class GUIRecatanglecalc implements ActionListener {
     public static void main(String[] args) {
 
         // generates JFrame
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Area of reactangle calc");
         JPanel panel = new JPanel();
         frame.setSize(100, 100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,24 +24,29 @@ public class GUIRecatanglecalc implements ActionListener {
         panel.setLayout(null);
 
         // creates info icon
-        userlabel = new JLabel("Input side a and side b to calculate area");
+        userlabel = new JLabel("Input both sides to calculate area");
         userlabel.setBounds(10, 20, 220, 25);
         panel.add(userlabel);
 
         // creates interface to input math variables in the string
         mathText = new JTextField();
-        mathText.setBounds(200, 20, 165, 25);
+        mathText.setBounds(250, 20, 165, 25);
         panel.add(mathText);
 
         // creates second interface to input math variables in the string
         mathtwoText = new JTextField();
-        mathtwoText.setBounds(200, 50, 165, 25);
+        mathtwoText.setBounds(250, 50, 165, 25);
         panel.add(mathtwoText);
 
         // creates button icon for calculations
         button = new JButton("Calculate");
         button.setBounds(10, 80, 120, 25);
         panel.add(button);
+
+        // sets result window
+        result = new JLabel("");
+        result.setBounds(150, 80, 300, 25);
+        panel.add(result);
 
         // Generates action for the button new method has to contain original method
         // name
@@ -59,5 +63,7 @@ public class GUIRecatanglecalc implements ActionListener {
         double area = sidea * sideb;
         System.out.println(sidea + " X " + sideb);
         System.out.println("Area: " + area);
+        result.setText("The area is: " + area);
+
     }
 }
