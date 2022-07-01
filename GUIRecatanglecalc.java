b08b02e3ef4721442e999adffcace9280f50fb2a
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-01 12:17:19 -0300

    Update GUIRecatanglecalc.java

--- GUIRecatanglecalc.java
+++ GUIRecatanglecalc.java
@@ -1,11 +1,11 @@
-
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class GUIRecatanglecalc implements ActionListener {
+    private static JTextField mathText;
+    private static JTextField mathtwoText;
 
     private static JLabel userlabel;
     private static JButton button;
@@ -27,18 +27,29 @@ public class GUIRecatanglecalc implements ActionListener {
         userlabel.setBounds(10, 20, 220, 25);
         panel.add(userlabel);
 
+        // creates interface to input math variables in the string
+        mathText = new JTextField();
+        mathText.setBounds(200, 20, 165, 25);
+        panel.add(mathText);
+
+        // creates second interface to input math variables in the string
+        mathtwoText = new JTextField();
+        mathtwoText.setBounds(200, 50, 165, 25);
+        panel.add(mathtwoText);
+
         // creates button icon for calculations
         button = new JButton("Calculate");
         button.setBounds(10, 80, 120, 25);
         panel.add(button);
 
-        // Generates action for the button new method has to contain original method name
+        // Generates action for the button new method has to contain original method
+        // name
         button.addActionListener(new GUIRecatanglecalc());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("Yes: " + countyes);
-        countyes++; //Used only to test if it is working properly
+        countyes++; // Used only to test if it is working properly
     }
 }
