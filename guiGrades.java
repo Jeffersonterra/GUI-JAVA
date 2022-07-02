b741a035964a88216ab91482957ce53f4d5d8661
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-02 19:27:12 -0300

    Update guiGrades.java

--- guiGrades.java
+++ guiGrades.java
@@ -22,19 +22,28 @@ public class guiGrades {
         JPanel panel = new JPanel();
         gradeone = new JTextField();
         ImageIcon image = new ImageIcon("logo.jnlp");// not working properly
-        JLabel label = new JLabel("Grades will be calculated and Students for all students");
-        label.setIcon(image);// not working properly
         frame.setSize(600, 600);
+        JLabel label = new JLabel("Grades will be calculated and Students for all students");
         frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// makes it so it closes after clicking x
-                                                             // can also do nothing or just hide
+        // can also do nothing or just hide
+
+        // creates point that prints if student has passed or not
+        JLabel passFail = new JLabel("");
+        passFail.setBounds(400, 0, 100, 25);
+        panel.add(passFail);
+
         frame.setVisible(true);// makes frame visible
         frame.setTitle("Grades");// sets title
         frame.add(panel);
         frame.setResizable(false);// makes it so it can not be resized
         panel.setLayout(null);
 
-        // creates second interface to input math variables in the string
+        // sets label
+        label.setBounds(0, 0, 260, 25);
+        label.setIcon(image);// not working properly
+
+        // creates interface to input math variables in the string
         gradeone = new JTextField();
         gradeone.setBounds(250, 50, 165, 25);
         panel.add(gradeone);
@@ -44,12 +53,12 @@ public class guiGrades {
         gradeTwo.setBounds(250, 100, 165, 25);
         panel.add(gradeTwo);
 
-        // creates second interface to input math variables in the string
+        // creates third interface to input math variables in the string
         gradeThree = new JTextField();
         gradeThree.setBounds(250, 150, 165, 25);
         panel.add(gradeThree);
 
-        // creates second interface to input math variables in the string
+        // creates fourth interface to input math variables in the string
         GradeFour = new JTextField();
         GradeFour.setBounds(250, 200, 165, 25);
         panel.add(GradeFour);
