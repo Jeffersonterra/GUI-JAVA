e9ad50a3105d7b7a84de3ea5502c230cfff85ca1
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-02 19:11:26 -0300

    Update guiGrades.java

--- guiGrades.java
+++ guiGrades.java
@@ -2,15 +2,29 @@ package GUI;
 
 import javax.swing.JFrame;
 
+import java.awt.Color;
+import javax.swing.*;
+
 import javax.swing.ImageIcon;
 
 import javax.swing.JPanel;
 
+import javax.swing.JLabel;
+
 public class guiGrades {
+    private static JTextField gradeone;
+    private static JTextField gradeTwo;
+    private static JTextField gradeThree;
+    private static JTextField gradefour;
     public static void main(String[] args) {
         JFrame frame = new JFrame();// creates frame and can name frame
         JPanel panel = new JPanel();
+        gradeone = new JTextField();
+        ImageIcon image = new ImageIcon("logo.jnlp");// not working properly
+        JLabel label = new JLabel("Grades will be calculated and Students for all students");
+        label.setIcon(image);// not working properly
         frame.setSize(600, 600);
+        frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// makes it so it closes after clicking x
                                                              // can also do nothing or just hide
         frame.setVisible(true);// makes frame visible
@@ -18,8 +32,11 @@ public class guiGrades {
         frame.add(panel);
         frame.setResizable(false);// makes it so it can not be resized
         panel.setLayout(null);
-        ImageIcon image = new ImageIcon("logo.png");// Creates icon logo
-        frame.setIconImage(image.getImage());// Sets icon logo
+
+        // creates second interface to input math variables in the string
+        gradeone = new JTextField();
+        gradeone.setBounds(250, 50, 165, 25);
+        panel.add(gradeone);
 
     }
 }
