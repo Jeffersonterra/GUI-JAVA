1e23ee2535a69124395a08922e173ceeb5ae0130
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-02 19:43:21 -0300

    Update guiGrades.java

--- guiGrades.java
+++ guiGrades.java
@@ -1,18 +1,22 @@
 package GUI;
 
-import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class guiGrades {
-    private static JTextField gradeone;
+public class guiGrades implements ActionListener {
+    private static JTextField gradeOne;
     private static JTextField gradeTwo;
     private static JTextField gradeThree;
-    private static JTextField GradeFour;
+    private static JTextField gradeFour;
+    private static JButton button;
+
+    private static JLabel passFail;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();// creates frame and can name frame
         JPanel panel = new JPanel();
-        gradeone = new JTextField();
+        gradeOne = new JTextField();
         ImageIcon image = new ImageIcon("logo.jnlp");// not working properly
         frame.setSize(600, 600);
         JLabel label = new JLabel("Grades will be calculated and Students for all students");
@@ -21,7 +25,7 @@ public class guiGrades {
         // can also do nothing or just hide
 
         // creates point that prints if student has passed or not
-        JLabel passFail = new JLabel("");
+        passFail = new JLabel("");
         passFail.setBounds(400, 0, 100, 25);
         panel.add(passFail);
 
@@ -36,9 +40,9 @@ public class guiGrades {
         label.setIcon(image);// not working properly
 
         // creates interface to input math variables in the string
-        gradeone = new JTextField();
-        gradeone.setBounds(250, 50, 165, 25);
-        panel.add(gradeone);
+        gradeOne = new JTextField();
+        gradeOne.setBounds(250, 50, 165, 25);
+        panel.add(gradeOne);
 
         // creates second interface to input math variables in the string
         gradeTwo = new JTextField();
@@ -51,9 +55,44 @@ public class guiGrades {
         panel.add(gradeThree);
 
         // creates fourth interface to input math variables in the string
-        GradeFour = new JTextField();
-        GradeFour.setBounds(250, 200, 165, 25);
-        panel.add(GradeFour);
+        gradeFour = new JTextField();
+        gradeFour.setBounds(250, 200, 165, 25);
+        panel.add(gradeFour);
 
+        // creates button icon for calculations
+        button = new JButton("Calculate");
+        button.setBounds(10, 80, 120, 25);
+        panel.add(button);
+
+        // Generates action for the button to calculate
+        button.addActionListener(new guiGrades());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String math = gradeOne.getText();
+        String mathtwo = gradeTwo.getText();
+        String maththree = gradeThree.getText();
+        String mathFour = gradeFour.getText();
+
+        double onegrade = Double.valueOf(math);
+        double twograde = Double.valueOf(mathtwo);
+        double threegrade = Double.valueOf(maththree);
+        double fourgrade = Double.valueOf(mathFour);
+
+        double Average = (onegrade + twograde + threegrade + fourgrade) / 4;
+
+        System.out.println("Grade one: " + onegrade);
+        System.out.println("Grade two: " + twograde);
+        System.out.println("Grade three: " + threegrade);
+        System.out.println("Grade four: " + fourgrade);
+        System.out.println("Avarege : " + Average);
+        if (Average >= 7) {
+            passFail.setText("PASSED");
+
+        } else {
+            passFail.setText("FAILED");
+        }
     }
 }
