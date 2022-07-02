5b188e6ad3be211a176b642d46477c7aa7d1673e
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-02 16:32:27 -0300

    Create guiGrades.java

---
+++ guiGrades.java
@@ -0,0 +1,16 @@
+
+
+import javax.swing.*;
+
+public class guiGrades {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Grades");
+        JPanel panel = new JPanel();
+        frame.setSize(100, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+        panel.setLayout(null);
+
+    }
+}
