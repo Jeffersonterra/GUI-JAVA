81ed9f337c4b37ce4b72cca746bf8785b65fd93c
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-08-04 12:58:14 -0300

    Update market.java

--- GUIMART/market.java
+++ GUIMART/market.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class market {
+
+    private static JTextField item1;
+
     public static void main(String args[]) {
         JFrame frame = new JFrame();// creates frame and can name frame
         JPanel panel = new JPanel();
@@ -14,5 +17,13 @@ public class market {
         frame.add(panel);
         frame.setResizable(true);// makes it so it can not be resized
         panel.setLayout(null);
+        frame.setSize(360, 300);
+
+        JLabel label = new JLabel("Duper Mart tell us what you bought today: ");
+        // creates interface to input temperature
+        item1 = new JTextField();
+        item1.setBounds(100, 150, 165, 25);
+        panel.add(item1);
+
     }
 }
